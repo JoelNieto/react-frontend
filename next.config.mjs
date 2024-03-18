271da2ad2cabb2b3aa29eadf22b2eb24327bae02
Joel Nieto <joelnietodev@gmail.com>
2024-03-18 09:42:50 -0500

    Adding images sources to config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dummyimage.com",
+        port: "",
+        pathname: "/150x150/**",
+      },
+      {
+        protocol: "https",
+        hostname: "dummyimage.com",
+        port: "",
+        pathname: "/350x350/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
